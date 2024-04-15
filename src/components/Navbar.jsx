aad2a69424c39ad7bec1ac1117655cda6b484615
cleznor <165759591+cleznor@users.noreply.github.com>
2024-04-15 06:38:27 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,11 +7,11 @@ const Navbar = (props) => {
             <div className='nav' style={{ textAlign: "right" }}>
                 <div className="navbar-brand" style={{ display: 'inline-block' }}>Sous Vide Recipes</div>
                 <ul className="navbar-nav" style={{ display: 'inline-block', listStyle: 'none', padding: 0 }}>
-                    <li style={{ display: 'inline-block', marginRight: '40px', borderRadius: '20px', border: '1px solid blue', padding: '8px 12px', backgroundColor: 'whitesmoke' }}><Link to="/" >Home</Link></li>
-                    <li style={{ display: 'inline-block', marginRight: '40px', borderRadius: '20px', border: '1px solid blue', padding: '8px 12px', backgroundColor: 'whitesmoke' }}><Link to="/form" >Add new item</Link></li>
-                    <li style={{ display: 'inline-block', color: "blue", borderRadius: '20px', border: '1px solid blue', padding: '8px 12px', backgroundColor: 'whitesmoke' }}><p className="nav-link" >Logged in as {props.user}</p></li>
+                    <li style={{ display: 'inline-block', marginRight: '40px', borderRadius: '20px', padding: '8px 12px', backgroundColor: 'whitesmoke' }}><Link to="/" >Home</Link></li>
+                    <li style={{ display: 'inline-block', marginRight: '40px', borderRadius: '20px', padding: '8px 12px', backgroundColor: 'whitesmoke' }}><Link to="/form" >Add new item</Link></li>
+                    <li style={{ display: 'inline-block', color: "blue", borderRadius: '20px', padding: '1px 12px', backgroundColor: 'whitesmoke' }}><p className="nav-link" >Logged in as {props.user}</p></li>
                     <div>
-					<li style={{ display: 'inline-block', color: "#262626", borderRadius: '20px', border: '1px solid blue', padding: '8px 12px', backgroundColor: 'whitesmoke', width: '120px', marginRight: '40px' }}><Link to="/" onClick={props.logout}>Logout</Link></li>
+					<li style={{ display: 'inline-block', color: "#262626", borderRadius: '20px', padding: '8px 12px', backgroundColor: 'whitesmoke', marginRight: '1px' }}><Link to="/" onClick={props.logout}>Logout</Link></li>
                     </div>
                 </ul>
             </div>
